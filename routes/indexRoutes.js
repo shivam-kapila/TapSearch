63247035e0159c517a66b484f47ca835412424fb
shivam-kapila <shivamkapila4@gmail.com>
2019-12-16 18:32:13 +0530

    Word Inverted Index Built

--- routes/indexRoutes.js
+++ routes/indexRoutes.js
@@ -2,24 +2,46 @@ var express = require("express"),
     router = express.Router();
     
 var input_text,
-    docs = new Map();
+    docs = {},
+    words = new Map();
 
 router.get("/",  (req, res) => {
     res.render('home');
 });
 
-router.get("/index", indexDocuments, (req, res) => {
+router.get("/index", indexDocuments, indexWords, (req, res) => {
     res.render('home');
-});
+})
 
 function indexDocuments(req, res, next){
-    input_text = req.query.input_text.split("\r\n\r\n");
+    input_text = req.query.input_text;
+    input_text_split = input_text.split("\r\n\r\n");
     i = 0;
-    input_text.forEach((inp) =>{
-        docs.set(i, input_text[i]);
+    input_text_split.forEach((inp) =>{
+        docs[i] = input_text_split[i];
         i++;
     })
-    console.log(docs)
+    // console.log(docs)
+    return next();
+}
+
+function indexWords(req, res, next){
+    // console.log(docs)
+    for (let current_doc_index  in docs){
+        current_doc = docs[current_doc_index];
+        console.log(current_doc)
+        current_doc_words = current_doc.split(" ");
+        current_doc_words.forEach((current_word) => {
+            doc_indices = []
+            if (!words.has(current_word)) {
+                words.set(current_word, []);
+            }
+            if (words.get(current_word).indexOf(current_doc_index) == -1){
+                words.get(current_word).push(current_doc_index);
+            }
+        });
+    }
+    // console.log(words)
     return next();
 }
 
